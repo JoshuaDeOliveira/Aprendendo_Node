8972241adc0f6d4827828c7b9242a514900fbc64
Joshua Erbe <joshuadultra@gmail.com>
2026-03-27 23:30:06 -0300

    Inserido no sistema as features de Deletar e Visualizar o banco atraves do usuario no terminal

--- index.js
+++ index.js
@@ -18,9 +18,23 @@ try {
 
     switch (Escolha) {
         case 'deletar':
-            console.log('Deletar usuario no banco')
+            const NomeUser = prompt()('Digite o nome do usuario a ser deletado: ')
+
+            const [VerificacaoResposta] = await connection.query(
+                `SELECT * FROM meus_amigos WHERE nomes = ?`, [NomeUser]
+            )
+
+            if (VerificacaoResposta.length > 0) {
+                await connection.query(
+                    `DELETE FROM meus_amigos WHERE nomes = ?`, [NomeUser]
+                )
+                console.log('Usuario Deletado com sucesso')
+            } else {
+                console.log('Usuario não localizado no banco de dados')
+            }
             break;
         case 'inserir':
+
             const Nome = prompt()('Digite o seu nome: ')
             const idade = prompt()('Digite a sua idade: ')
             const sexualidade = prompt()('Digite a sua sexualidade: ')
@@ -38,11 +52,14 @@ try {
             console.log(`esses dados foram inseridos`, Respostas)
 
             break;
+
         case 'atualizar':
-            console.log('atualizar algo no banco')
             break;
         case 'visualizar':
-            console.log('visualizar dados da tabela')
+            const [Dados] = await connection.query(
+                'SELECT * FROM meus_amigos'
+            )
+            console.log('Esses são os dados da tabela ', Dados)
             break;
         default:
             console.log('Comando incorreto, digite novamente')
