6f3b290e74ce2ded919a3517490b2109c556ded3
Joshua Erbe <joshuadultra@gmail.com>
2026-03-27 22:51:27 -0300

    Atualização de Segurança contra SQL Injection (querries parametrizadas) inseridas + prototipo para novas funcionalidades

--- index.js
+++ index.js
@@ -2,46 +2,62 @@ import mysql from 'mysql2/promise';
 import prompt from 'prompt-sync';
 import 'dotenv/config';
 
+
 async function TesteBanco() {
-    const connection = await mysql.createConnection({
-    host: process.env.DBHOST,
-    user: process.env.DBUSER,
-    password: process.env.DBPASSWORD,
-    database: process.env.DBDATABASE
-})
-
+    var connection;
 try {
-    const Nome = prompt()('Digite o seu nome: ')
-    const idade = prompt()('Digite a sua idade: ')
-    const sexualidade = prompt()('Digite a sua sexualidade: ')
-    await connection.query(
-        `INSERT INTO meus_amigos (nomes, idades, sexualidade)
-        VALUES ('${Nome}', '${idade}', '${sexualidade}')`
-    );
-    console.log('Dados foram inseridos com sucesso!')
+    connection = await mysql.createConnection({
+        host: process.env.DBHOST,
+        user: process.env.DBUSER,
+        password: process.env.DBPASSWORD,
+        database: process.env.DBDATABASE 
+    })
+    
+    const Ação = prompt()('O que gostaria de fazer na tabela de amigos? (INSERIR, DELETAR, ATUALIZAR OU APENAS VISUALIZAR) ')
+    const Escolha = Ação.toLowerCase()
 
-    const [Respostas] = await connection.query(
-        'SELECT * FROM meus_amigos'
-    )
-    console.log(`esses dados foram inseridos`, Respostas)
-} catch (ErroBanco) {
-    console.log(`${ErroBanco} - foi isso que aconteceu na hora de inserir os dados`)
+    switch (Escolha) {
+        case 'deletar':
+            console.log('Deletar usuario no banco')
+            break;
+        case 'inserir':
+            const Nome = prompt()('Digite o seu nome: ')
+            const idade = prompt()('Digite a sua idade: ')
+            const sexualidade = prompt()('Digite a sua sexualidade: ')
+            await connection.query(
+                `INSERT INTO meus_amigos (nomes, idades, sexualidade)
+                VALUES (?, ?, ?)`, [Nome, idade, sexualidade]
+            );
+    
+            console.log('Dados foram inseridos com sucesso!')
+
+            const [Respostas] = await connection.query(
+                'SELECT * FROM meus_amigos'
+            )
+
+            console.log(`esses dados foram inseridos`, Respostas)
+
+            break;
+        case 'atualizar':
+            console.log('atualizar algo no banco')
+            break;
+        case 'visualizar':
+            console.log('visualizar dados da tabela')
+            break;
+        default:
+            console.log('Comando incorreto, digite novamente')
+            break;
+    } 
+
+} catch (err) {
+    if (err.massage == 'ECONNREFUSED'){
+        console.log('Não foi possivel estabelecer uma conexão ao banco')
+    } else {
+        console.log(`Esse é o codigo do erro encontrado ${err.code} e essa é a mensagem ${err.message}`)
+    }
 } finally {
-    await connection.end();
-} 
-
-/*try{
-    const [Respostas] = await connection.query(
-        'SELECT * FROM meus_amigos'
-    );
-    console.log(Respostas)
-    console.log('Funcionou!')
-} catch (ErroBanco) {
-    console.log(`${ErroBanco} - foi essa merda que aconteceu`)
-} finally {
-    await connection.end();
-}*/
-
+    if (connection) await connection.end()
+}
 }
 
 TesteBanco();
